07a139110e28dd247d1e61c679c8ae0c2bcbf537
Matthew Sage <mattsage@users.noreply.github.com>
2017-03-22 14:52:20 +0000

    Create for.py

---
+++ Python/for.py
@@ -0,0 +1,8 @@
+for x in aSequence:
+ #do stuff for each member of aSequence
+ #for example, each item in a list, each
+ #character in a string, etc.
+ for x in range(10):
+ #do stuff 10 times (0 through 9)
+ for x in range(5,10):
+ #do stuff 5 times (5 through 9)

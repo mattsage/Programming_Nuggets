387236ed2110de3ebda05820f52bc55664103ce1
Matthew Sage <mattsage@users.noreply.github.com>
2017-03-22 14:55:40 +0000

    Create files.py

---
+++ Python/files.py
@@ -0,0 +1,6 @@
+thisfile = open(“datadirectory/file.txt”) 
+
+thisfile.read() #reads entire file into one string
+thisfile.readline() #reads one line of a file
+thisfile.readlines() #reads entire file into a list of strings, one per line
+for eachline in thisfile: #steps through lines in a file

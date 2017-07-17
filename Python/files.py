e22f26c7d3ba272bd069828cd31b4be15fee2fe0
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-17 14:09:31 +0100

    Update files.py

--- Python/files.py
+++ Python/files.py
@@ -4,3 +4,9 @@ thisfile.read() #reads entire file into one string
 thisfile.readline() #reads one line of a file
 thisfile.readlines() #reads entire file into a list of strings, one per line
 for eachline in thisfile: #steps through lines in a file
+
+  # Example:
+linestring = open('/home/pi/Pushbulletkey.config', 'r').read()
+
+# Then you can print it:
+print linestring
